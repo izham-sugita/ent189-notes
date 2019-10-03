0c52708e81a7acccef1e17149ade549d9bd2beeb
sugita <sugita5019@gmail.com>
2019-10-03 12:51:23 +0800

    updates

--- lab/snippets/macro-var.c
+++ lab/snippets/macro-var.c
@@ -4,6 +4,12 @@
 
 int main()
 {
+  /*Code specifier Lg, e*/
+  
   printf("Max for long double: %Lg\n", LDBL_MAX);
   printf("Min for long double: %Lg\n", LDBL_MIN);
+
+  printf("Min for long double: %e\n", DBL_MIN);
+  printf("Max for long double: %e\n", DBL_MAX);
+
 }
