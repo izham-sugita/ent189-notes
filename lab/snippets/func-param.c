8bcc6bbf2580666b56a7b70163300d27791a654d
sugita <sugita5019@gmail.com>
2019-10-09 23:24:30 +0800

    update

--- lab/snippets/func-param.c
+++ lab/snippets/func-param.c
@@ -3,6 +3,7 @@
 
 int f0(int);
 int f1(int *);
+int f2(int *a);
 
 int main()
 {
@@ -12,8 +13,13 @@ int main()
   f0(a);
   printf("Value of a=%d after f0 call.\n", a);
 
+  a = 1; //re-initiate
   f1(&a);
   printf("Value of a=%d after f1 call.\n", a);
+
+  a = 1; //re-initiate
+  f2(&a);
+  printf("Value of a=%d after f2 call.\n", a);
   
 }
 
@@ -25,4 +31,11 @@ int f0(int a)
 int f1(int *a)
 {
   *a = *a + 1;
+}
+
+int f2(int *a)
+{
+  int temp = *a;
+  temp = temp + 1;
+  *a = temp;
 }
