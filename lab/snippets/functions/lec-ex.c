ec1f7f44011df8497545aecd98ef59fdd560bb58
sugita <sugita5019@gmail.com>
2020-12-30 17:14:50 +0800

    lecture

---
+++ lab/snippets/functions/lec-ex.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void menu();
+float determine_price(int);
+float calc(float,int);
+void print_result(float);
+
+int main()
+{
+
+  int code, qty;
+  float price;
+  
+  menu();
+  printf("Enter item's code and quantity\n");
+  scanf("%d %d", &code, &qty);
+  price = determine_price(code);
+  printf("price = %f\n", price);
+  
+
+  
+}
+
+void menu()
+{
+  printf("Code\tItem\tPrice\n");
+  printf("1\tPapaya\t1.00\n");
+  printf("2\tMelon\t2.00\n");
+  printf("3\tApple\t5.00\n");
+}
+
+float determine_price(int a){
+  float pricing;
+  switch(a){
+  case 1:
+    pricing = 1.00; break;
+  case 2:
+    pricing = 2.00; break;
+  case 3:
+    pricing = 5.00; break;
+  }
+  return (pricing);
+}
